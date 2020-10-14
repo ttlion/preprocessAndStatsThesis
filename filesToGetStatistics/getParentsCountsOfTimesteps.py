97aa7494ae6fc4ba473a3c020091cbc57490994f
ttlion <tiago.miguel.leao@gmail.com>
2020-10-14 15:06:49 +0100

    getParentsCountsOfTimesteps with user interface

--- filesToGetStatistics/getParentsCountsOfTimesteps.py
+++ filesToGetStatistics/getParentsCountsOfTimesteps.py
@@ -11,6 +11,68 @@ import re
 
 ###########################################################################
 ###########################################################################
+# Defining proper command-line user interface
+
+def showUsage():
+    print('usage: python getParentsCountsOfTimesteps.py parentsToCheck.csv dbnsToUse.csv timestepToCheck \n')
+    print('Program counts, considering the DBNs in dbnsToUse.csv, the number of children each variable from parentsToCheck.csv has in the timestep timestepToCheck\n')
+    print('parentsToCheck.csv: has a line with all variables for which the user wants to know the number of children')
+    print('dbnsToUse.csv: has the DBNs to consider')
+    print('timestepToCheck: has the desired timestep')
+    print('\nRun python getParentsCountsOfTimesteps.py -eParents for an example of parentsToCheck.csv')
+    print('\nRun python getParentsCountsOfTimesteps.py -eDBNs for an example of dbnsToUse.csv')
+    print('\nRun python getParentsCountsOfTimesteps.py -h for usage')
+    return
+
+def printExampleParents():
+    print('This file has a single line with the variables for which the user is interested in knowing the number of children (in the specified timestep).')
+    print('The variables can be static or dynamic.')
+    print('Example of a file, for dynamic variables X, Y and Z, and static variables A and B:\n')
+    print('X,Y,Z,A,B')
+    return
+
+def printExampleDBNs():
+    print('This file must have the DBNs that must be considered for making the counts.')
+    print('The DBNs must be sdtDBNs stored in a file, see the sdtDBN webpage for more details.')
+    print('This file may have multiple lines, in that case it will perform the counts for each set of DBNs in each line.')
+    print('Each line must have a tag associated, which is done by putting the names of the DBNs of each line always in the format tag_someDesiredName')
+    print('An example would be the following:\n')
+    print('tag1_someStoredDBN1.txt\ntag2_someStoredDBN2.txt,tag2_someOtherStoredDBN2.txt\n')
+    print('In the previous example, the program would output the counts for tag1 and tag2.')
+    print('The counts associated with tag1 would be of the DBN stored in tag1_someStoredDBN1.txt')
+    print('The counts associated with tag2 would be of the DBNs stored in tag2_someStoredDBN2.txt and tag2_someOtherStoredDBN2.txt')
+    return
+
+# Check command-line arguments validity
+if len(sys.argv) == 2:
+    if sys.argv[1] == '-h':
+        showUsage()
+        exit()
+    elif sys.argv[1] == '-eParents':
+        printExampleParents()
+        exit()
+    elif sys.argv[1] == '-eDBNs':
+        printExampleDBNs()
+        exit()
+
+if len(sys.argv) != 4:
+    print('Not all arguments inserted! Run python getParentsCountsOfTimesteps.py -h for usage')
+    exit()
+
+if sys.argv[1].find('.csv') == -1:
+    print('Input file parentsToCheck.csv must be .csv format! Run python getParentsCountsOfTimesteps.py -h for usage')
+    exit()
+
+if sys.argv[2].find('.csv') == -1:
+    print('Input file dbnsToUse.csv must be .csv format! Run python getParentsCountsOfTimesteps.py -h for usage')
+    exit()
+
+if int(sys.argv[3]) < 0:
+    print('Timestep cannot be < 0 ! Run python getParentsCountsOfTimesteps.py -h for usage')
+    exit()
+
+###########################################################################
+###########################################################################
 # Ler ficheiros de input
 
 parentsToCheck = { }
